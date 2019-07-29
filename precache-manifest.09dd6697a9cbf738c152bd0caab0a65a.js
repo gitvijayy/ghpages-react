b0db5a5b5196e295319362dcc7fc44c131a37a6e
gitvijayy <7822vijay@gmail.com>
2019-07-29 16:00:45 -0700

    Updates

---
+++ precache-manifest.09dd6697a9cbf738c152bd0caab0a65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f8995ea613c9fde2ca10bc0cbe2a3d",
+    "url": "/gitvijayy/ghpages-react/index.html"
+  },
+  {
+    "revision": "a56ec514f68cf6379675",
+    "url": "/gitvijayy/ghpages-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gitvijayy/ghpages-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a56ec514f68cf6379675",
+    "url": "/gitvijayy/ghpages-react/static/js/main.2bc37c2b.chunk.js"
+  },
+  {
+    "revision": "57c7bd15fa37a2631168",
+    "url": "/gitvijayy/ghpages-react/static/js/runtime~main.2c3324e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitvijayy/ghpages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gitvijayy/ghpages-react/precache-manifest.09dd6697a9cbf738c152bd0caab0a65a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gitvijayy/ghpages-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
